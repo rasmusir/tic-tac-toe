de50b5a0e78851c01eed3a80faf23074a36cfa3d
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2018-04-10 22:23:03 +0200

    import websocket

--- source/server/server.js
+++ source/server/server.js
@@ -1,6 +1,7 @@
 //NOTE import libraries/modules
 const express = require("express")
 const http = require("http")
+const WebSocket = require("ws")
 
 //NOTE create server "app"
 const app = express()
@@ -10,5 +11,19 @@ const server = http.createServer(app)
 
 app.use("/script/", express.static("source/client/"))
 app.use("/", express.static("public/views/"))
+
+const webSocketServer = new WebSocket.Server({ server })
+
+webSocketServer.on("connection", client => {
+    console.log("Someone connected :D")
+
+    client.on("message", message => {
+        console.log("Someone said: " + message)
+    })
+
+    client.on("close", () => {
+        console.log("Someone left :(")
+    })
+})
 
 server.listen(9080, () => console.log("Listening on port 9080"))
